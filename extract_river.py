d331ce5de486f1079a6ce8847a7d1a13efdc3c20
Martin Graive <martin.graive@eleves.enpc.fr>
2020-05-28 10:15:02 +1000

    Take exactly one before START_TIME and one after STOP_TIME

--- extract_river.py
+++ extract_river.py
@@ -19,71 +19,43 @@ if not os.path.isdir(args.pout):
 
 convert = lambda d, u1, u2: cftime.date2num(cftime.num2date(d, u1), u2)
 
-headerf = ("## COLUMNS 2\n##\n## COLUMN1.name time\n## COLUMN1.long_name Time\n"
-"## COLUMN1.units {}\n##\n## COLUMN2.name flow\n"
-"## COLUMN2.long_name River flow\n## COLUMN2.units m3s-1\n".format(args.unit))
-headert = ("## COLUMNS 2\n##\n## COLUMN1.name time\n## COLUMN1.long_name Time\n"
+header = {'flow': ("## COLUMNS 2\n##\n## COLUMN1.name time\n"
+"## COLUMN1.long_name Time\n## COLUMN1.units {}\n##\n## COLUMN2.name flow\n"
+"## COLUMN2.long_name River flow\n## COLUMN2.units m3s-1\n".format(args.unit)),
+'temp': ("## COLUMNS 2\n##\n## COLUMN1.name time\n## COLUMN1.long_name Time\n"
 "## COLUMN1.units {}\n##\n## COLUMN2.name temp\n"
-"## COLUMN2.long_name Temperature\n## COLUMN2.units degC\n".format(args.unit))
+"## COLUMN2.long_name Temperature\n## COLUMN2.units degC\n".format(args.unit))}
 
-for path in glob(os.path.join(args.pin, "*flow.ts")):
-    with open(path, 'r') as fi:
+for vname in ['temp', 'flow']:
+    for path in glob(os.path.join(args.pin, "*{}.ts".format(vname))):
         with open(os.path.join(args.pout, os.path.basename(path)), 'w') as fo:
-            fo.write(headerf)
-            it = -2
-            for line in fi:
-                if line.startswith('#'):
-                    matcht = re.search("COLUMN(\d+).name\s+time", line)
-                    if matcht:
-                        it = int(matcht.group(1)) - 1
-                    matchu = re.search("UMN{}.units\s+(.*)$".format(it+1), line)
-                    if matchu:
-                        in_units = matchu.group(1)
-                        start_date = convert(args.sd, args.unit, in_units)
-                        end_date = convert(args.ed, args.unit, in_units)
-                        ref = cftime.date2num(cftime.datetime(2016, 1, 1),
-                                              in_units)  # fixme
-                        shift = ((start_date - ref) // 365.25) * 365.25
-                        new_start = start_date - shift
-                        new_end = end_date - shift
-                    matchv = re.search("COLUMN(\d+).name\s+flow", line)
-                    if matchv:
-                        iv = int(matchv.group(1)) - 1
-                else:
-                    date, val = float(line.split()[it]), line.split()[iv]
-                    if date >= new_start - .5:
-                        fo.write(str(convert(date+shift, in_units, args.unit)) +
-                                 " " + val + "\n")
-                        if date > new_end:
-                            break
-
-for path in glob(os.path.join(args.pin, "*temp.ts")):
-    with open(path, 'r') as fi:
-        with open(os.path.join(args.pout, os.path.basename(path)), 'w') as fo:
-            fo.write(headert)
-            it = -2
-            for line in fi:
-                if line.startswith('#'):
-                    matcht = re.search("COLUMN(\d+).name\s+time", line)
-                    if matcht:
-                        it = int(matcht.group(1)) - 1
-                    matchu = re.search("UMN{}.units\s+(.*)$".format(it+1), line)
-                    if matchu:
-                        in_units = matchu.group(1)
-                        start_date = convert(args.sd, args.unit, in_units)
-                        end_date = convert(args.ed, args.unit, in_units)
-                        ref = cftime.date2num(cftime.datetime(2016, 1, 1),
-                                              in_units)  # fixme
-                        shift = ((start_date - ref) // 365.25) * 365.25
-                        new_start = start_date - shift
-                        new_end = end_date - shift
-                    matchv = re.search("COLUMN(\d+).name\s+temp", line)
-                    if matchv:
-                        iv = int(matchv.group(1)) - 1
-                else:
-                    date, val = float(line.split()[it]), line.split()[iv]
-                    if date >= new_start - .5:
-                        fo.write(str(convert(date+shift, in_units, args.unit)) +
-                                 " " + val + "\n")
-                        if date > new_end:
-                            break
+            with open(path, 'r') as fi:
+                fo.write(header[vname])
+                it = -2
+                prev = 0, "0"
+                for line in fi:
+                    if line.startswith('#'):
+                        matcht = re.search("N(\d+).name time", line)
+                        if matcht:
+                            it = int(matcht.group(1))
+                        matchu = re.search("N{}.units (.*)$".format(it), line)
+                        if matchu:
+                            in_units = matchu.group(1)
+                            start_date = convert(args.sd, args.unit, in_units)
+                            end_date = convert(args.ed, args.unit, in_units)
+                            ref = cftime.date2num(cftime.datetime(2016, 1, 1),
+                                                  in_units)  # fixme
+                            shift = ((start_date - ref) // 365.25) * 365.25
+                            new_start = start_date - shift
+                            new_end = end_date - shift
+                        matchv = re.search("N(\d+).name {}".format(vname), line)
+                        if matchv:
+                            iv = int(matchv.group(1)) - 1
+                    else:
+                        date, val = float(line.split()[it-1]), line.split()[iv]
+                        if date >= new_start:
+                            fo.write(str(convert(prev[0] + shift, in_units, args.unit)) + " " + prev[1] + "\n")
+                            if date > new_end:
+                                fo.write(str(convert(date + shift, in_units, args.unit)) + " " + val + "\n")
+                                break
+                        prev = date, val
