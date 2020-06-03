fd164b64add8ab569b5a7361f4527e4caf474412
MartinGraive <mgraive@hpc-login.aims.gov.au>
2020-06-03 14:19:57 +1000

    Add possibility to choose filename of output

--- om2mom4hf.sh
+++ om2mom4hf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Usage: ./om2mom4hf.sh file minlon maxlon minlat maxlat"
+echo "Usage: ./om2mom4hf.sh file minlon maxlon minlat maxlat [outfile]"
 echo ""
+
+FILE=${6:-heatflux.nc}
 
 $( dirname "$0" )/om2splus.sh $1 swflx $2 $3 $4 $5
 $( dirname "$0" )/om2splus.sh $1 lw_heat $2 $3 $4 $5
@@ -13,18 +15,18 @@ ncrename -v sens_heat,sensible sens_heat-mod.nc
 ncrename -v evap_heat,latent evap_heat-mod.nc
 
 ncap2 -A -s "lwr=lw_heat*(-1)" lw_heat-mod.nc 
-ncks -x -v lw_heat lw_heat-mod.nc heatflux.nc
-ncatted -O -a long_name,lwr,o,c,"upward long wave flux" heatflux.nc
-ncatted -O -a standard_name,lwr,d,, heatflux.nc
+ncks -x -v lw_heat lw_heat-mod.nc $FILE
+ncatted -O -a long_name,lwr,o,c,"upward long wave flux" $FILE
+ncatted -O -a standard_name,lwr,d,, $FILE
 
-ncks -A swflx-mod.nc heatflux.nc
-ncks -A sens_heat-mod.nc heatflux.nc
-ncks -A evap_heat-mod.nc heatflux.nc
+ncks -A swflx-mod.nc $FILE
+ncks -A sens_heat-mod.nc $FILE
+ncks -A evap_heat-mod.nc $FILE
 
-ncatted -O -a units,sensible,o,c,"W m-2" heatflux.nc
-ncatted -O -a units,lwr,o,c,"W m-2" heatflux.nc
-ncatted -O -a units,swr,o,c,"Wm-2" heatflux.nc
-ncatted -O -a units,latent,o,c,"kg/m2/sec" heatflux.nc
+ncatted -O -a units,sensible,o,c,"W m-2" $FILE
+ncatted -O -a units,lwr,o,c,"W m-2" $FILE
+ncatted -O -a units,swr,o,c,"Wm-2" $FILE
+ncatted -O -a units,latent,o,c,"kg/m2/sec" $FILE
 
 rm swflx-mod.nc lw_heat-mod.nc sens_heat-mod.nc evap_heat-mod.nc
 
