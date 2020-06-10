9ad093d544cb126daeb15ba8d3a6dda3fed36773
MartinGraive <mgraive@hpc-login.aims.gov.au>
2020-06-10 16:52:18 +1000

    Lowercase variables

--- om2splus.sh
+++ om2splus.sh
@@ -5,20 +5,20 @@ if [ "$#" -lt 6 ]; then
    exit
 fi
 
-FILE=${7:-$2-mod.nc}
-UNITS=`ncdump -h $1 | grep "$2:units" | sed -E "s/.*$2\:units = \"(.+)\" ;/\1/"`
-LONG=`ncdump -h $1 | grep "$2:long_name" | sed -E "s/.*$2\:long_name = \"(.+)\" ;/\1/"`
-STANDARD=`ncdump -h $1 | grep "$2:standard_name" | sed -E "s/.*$2\:standard_name = \"(.+)\" ;/\1/"`
+file=${7:-$2-mod.nc}
+units=`ncdump -h $1 | grep "$2:units" | sed -E "s/.*$2\:units = \"(.+)\" ;/\1/"`
+long=`ncdump -h $1 | grep "$2:long_name" | sed -E "s/.*$2\:long_name = \"(.+)\" ;/\1/"`
+standard=`ncdump -h $1 | grep "$2:standard_name" | sed -E "s/.*$2\:standard_name = \"(.+)\" ;/\1/"`
 
-ncks -v $2 $1 $FILE
-ncrename -v $2,eta_t $FILE
-$(dirname "$0")/oceanmaps2shoc $FILE $2-mod-tmp.nc $3 $4 $5 $6 coord2d
-mv $2-mod-tmp.nc $FILE
-ncrename -v eta_t,$2 $FILE 
-ncatted -O -a units,$2,o,c,"$UNITS" $FILE
-ncatted -O -a long_name,$2,o,c,"$LONG" $FILE
-if [ "$STANDARD" == "" ]; then
-   ncatted -O -a standard_name,$2,d,, $FILE
-else ncatted -O -a standard_name,$2,o,c,"$STANDARD" $FILE
+ncks -v $2 $1 $file
+ncrename -v $2,eta_t $file
+$(dirname "$0")/oceanmaps2shoc $file $2-mod-tmp.nc $3 $4 $5 $6 coord2d
+mv $2-mod-tmp.nc $file
+ncrename -v eta_t,$2 $file 
+ncatted -O -a units,$2,o,c,"$units" $file
+ncatted -O -a long_name,$2,o,c,"$long" $file
+if [ "$standard" == "" ]; then
+   ncatted -O -a standard_name,$2,d,, $file
+else ncatted -O -a standard_name,$2,o,c,"$standard" $file
 fi
-ncatted -O -a valid_range,$2,d,, $FILE
+ncatted -O -a valid_range,$2,d,, $file
