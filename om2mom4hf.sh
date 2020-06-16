4b77435670a1755f29787c069c2f630d798f21bc
MartinGraive <mgraive@hpc-login.aims.gov.au>
2020-06-16 10:49:04 +1000

    Fix forgotten conversion (div by Lv)

--- om2mom4hf.sh
+++ om2mom4hf.sh
@@ -12,16 +12,17 @@ $( dirname "$0" )/om2splus.sh $1 evap_heat $2 $3 $4 $5
 
 ncrename -v swflx,swr swflx-mod.nc
 ncrename -v sens_heat,sensible sens_heat-mod.nc
-ncrename -v evap_heat,latent evap_heat-mod.nc
+
+ncap2 -A -s "latent=evap_heat/(2440000)" evap_heat-mod.nc
+ncks -x -v evap_heat evap_heat-mod.nc $FILE
 
 ncap2 -A -s "lwr=lw_heat*(-1)" lw_heat-mod.nc 
-ncks -x -v lw_heat lw_heat-mod.nc $FILE
+ncks -A -x -v lw_heat lw_heat-mod.nc $FILE
 ncatted -O -a long_name,lwr,o,c,"upward long wave flux" $FILE
 ncatted -O -a standard_name,lwr,d,, $FILE
 
 ncks -A swflx-mod.nc $FILE
 ncks -A sens_heat-mod.nc $FILE
-ncks -A evap_heat-mod.nc $FILE
 
 ncatted -O -a units,sensible,o,c,"W m-2" $FILE
 ncatted -O -a units,lwr,o,c,"W m-2" $FILE
